589cc822ed953f7da927d8433d4ae28833537d90
Tomasz Hanczyk <hanczykt@vp.pl>
2025-11-06 19:34:44 +0100

    feat: add DI container

---
+++ Order/Services/ServiceSupplier.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Order.Interfaces;
+using Order.Repositories;
+
+namespace Order.Services
+{
+	public static class ServiceSupplier
+	{
+		private static ServiceProvider _serviceProvider = Build();
+
+		private static ServiceProvider Build()
+		{
+			var services = new ServiceCollection();
+			services.AddTransient<IOrderService, OrderService>();
+			services.AddTransient<IOrderRepository, OrderRepository>();
+			services.AddTransient<ILogger, Logger>();
+			return services.BuildServiceProvider();
+		}
+
+		public static T? GetService<T>()
+		{
+			return (T?)_serviceProvider.GetService(typeof(T));
+		}
+
+		public static T GetRequiredService<T>()
+		{
+			return (T)_serviceProvider.GetRequiredService(typeof(T));
+		}
+	}
+}
